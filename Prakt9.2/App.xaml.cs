68baaa8a97f71a3f6b4f9f460cd4f996aae40bdc
Altari <zyw.err@mail.ru>
2023-11-27 03:06:25 +0300

    Добавьте файлы проекта.

---
+++ Prakt9.2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Prakt9._2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Prakt9.2/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Reflection;
+using System.Xml.Linq;
+
+
+namespace Prakt9._2
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        StreamReader inFile;
+        string mainDir = FileSystem.Current.AppDataDirectory;
+        StreamWriter outFile;
+        string folderPath;
+
+
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private string GetLocalFolderPath()
+        {
+            // Получение пути к локальному каталогу приложения
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            return folderPath;
+        }
+        private void BornDP_DateSelected(object sender, DateChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            var result = await MediaPicker.PickPhotoAsync();
+            if (result != null)
+            {
+                var selectedPhotoStream = await result.OpenReadAsync();
+                ImageView.Source = ImageSource.FromStream(() => selectedPhotoStream);
+            }
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+            StreamWriter outFile = new StreamWriter(Path.Combine(mainDir, "file.txt"));
+            outFile.WriteLine(Surname.Text);
+            outFile.Close();
+
+        }
+
+        private void ContentPage_Appearing(object sender, EventArgs e)
+        {
+
+            if (File.Exists(Path.Combine(folderPath, "file.txt")) == true)
+            {
+                StreamReader outFile = new StreamReader(Path.Combine(folderPath, "file.txt"));
+                Surname.Text = outFile.ReadLine();
+                outFile.Close();
+            }
+        }
+    }
+}
